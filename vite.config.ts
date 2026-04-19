d667f717ff999bbf5ff3ff50fb9c3800d43df37b
robson120-s <shroba391@gmail.com>
2026-04-19 14:52:17 +0300

    update v7

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,3 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  vite: {
-    build: {
-      outDir: "dist/client"
-    }
-  }
-});
+export default defineConfig({});
